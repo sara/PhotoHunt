897c49aeec598a6f163d0a9962ac2f94ac2a206a
SaraAnn Stanway <sarastanway@gmail.com>
2016-12-30 18:22:01 -0500

    first commit

---
+++ __init__.py
@@ -0,0 +1,4 @@
+from flask import Flask
+from flask.ext.mongoengine import MongoEngine
+app = Flask(__name__)
+app.config["\x8a\x13\x0c\xb2\xcf\x8c {I\x17Cz}\x19\xd6sR\x8b\xd8\xc6^P\xa2M"] = ''
---
+++ app.py
@@ -0,0 +1,7 @@
+from mongoengine import connect
+from flask.ext.mongoengine import MongoEngine
+
+DB_NAME = 'merp'
+DB_USERNAME = 'merp'
+DB_PASSWORD = 'blackjack10'
+DB_HOST_ADDRESS = 
